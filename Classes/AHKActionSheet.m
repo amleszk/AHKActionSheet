99c78618d5fa288e23ec6a4cc0b4142c8731c405
A M Leszkiewicz <amleszk@gmail.com>
2014-11-28 15:35:38 -0500

    Added safer teardown method

--- Classes/AHKActionSheet.m
+++ Classes/AHKActionSheet.m
@@ -330,10 +330,8 @@ static const CGFloat kCancelButtonShadowHeightRatio = 0.333f;
 
     void(^tearDownView)(void) = ^(void) {
         // remove the views because it's easiest to just recreate them if the action sheet is shown again
-        for (UIView *view in @[self.tableView, self.cancelButton, self.blurredBackgroundView, self.window]) {
-            [view removeFromSuperview];
-        }
-
+        [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+        [self.window removeFromSuperview];
         self.window = nil;
         [self.previousKeyWindow makeKeyAndVisible];
 
